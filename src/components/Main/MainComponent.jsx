461fc72782231d5aaecd186a957f3ca9c2cfefb6
Nastya <asekerich99@gmail.com>
2021-01-13 20:38:01 +0300

    init: janeellison dev branch

--- src/components/Main/MainComponent.jsx
+++ src/components/Main/MainComponent.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import SearchFood from './SearchFood/SearchFoodComponent';
+import ExersiseComponent from './Exersise/ExersiseComponent';
 
 const MainComponent = () => {
   return (
     <main>
       <Route exact path="/" render={() => <div>Default page</div>} />
       <Route path="/search_food" render={() => <SearchFood/>} />
-      <Route path="/todo" render={() => <div>Todo</div>} />
+      <Route path="/todo" render={() => <ExersiseComponent/>}/>
     </main>
   );
 };
