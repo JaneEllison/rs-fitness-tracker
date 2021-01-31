3ab579f226ed494840e70a576cac5169a7f8710d
JaneEllison <asekerich99@gmail.com>
2021-01-31 22:44:15 +0300

    feat: add ModalWeatherComponent and styles for weather

--- src/components/Header/Weather/WeatherComponent.jsx
+++ src/components/Header/Weather/WeatherComponent.jsx
@@ -1,61 +1,105 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col, Typography } from 'antd';
+import WeatherModalComponent from './WeatherModal/WeatherModalComponent'
 import weatherApi from '../../../api/makeQueryToShowWeather';
 import cityApi from '../../../api/makeQueryToFindCity';
 import style from './Weather.module.css';
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
 const WeatherComponent = () => {
-  const [cityInfo, setCityInfo] = useState({});
+  const [weatherInfo, setWeatherInfo] = useState({});
 
   const getWeatherForUser = async () => {
-    const responseIp = await cityApi.get('', {});
+    const ipData = await cityApi.get('', {});
 
-    let city = responseIp.data.city.name_en;
+    let city;
+    let weatherData;
 
-    const response = await weatherApi.get('', {
-      params: {
-        query: city,
-      },
-    });
+    (ipData)
+    ? city = ipData.data.city.name_en
+    : city = 'Minsk'
+    
+    //у апи есть ограничение на запросы, поэтому пока так
 
-    setCityInfo({
-      cityTemperature: response.data.current.temperature,
-      cityName: city,
-      cityDiscr: response.data.current.weather_descriptions[0],
-      cityIcon: response.data.current.weather_icons[0],
-    });
+    // weatherData = await weatherApi.get('', {
+    //   params: {
+    //     query: city,
+    //   },
+    // });
+
+    if (weatherData) {
+      setWeatherInfo({
+        temperature: weatherData.data.current.temperature,
+        name: city,
+        countryName: ipData.data.country.name_en,
+        discription: weatherData.data.current.weather_descriptions[0],
+        icon: weatherData.data.current.weather_icons[0],
+        temperatureFeelsLike: weatherData.data.current.feelslike, 
+        wind: weatherData.data.current.wind_speed,
+        precip: weatherData.data.current.precip,
+        humidity: weatherData.data.current.humidity,
+      });
+    } else {
+      setWeatherInfo({
+        temperature: '-3',
+        name: 'Minsk',
+        countryName: "Belarus",
+        discription: 'Mostly cloudy',
+        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW6Eqcl15XoN3eM922kfnx7ftuuENq5kTCrg&usqp=CAU',
+        temperatureFeelsLike: '-5', 
+        wind: '11',
+        precip: '3',
+        humidity: '88',
+      });
+    }
   };
 
-  // useEffect(() => {
-  //   getWeatherForUser();
-  // }, []);
+  useEffect(() => {
+    getWeatherForUser();
+  }, []);
+
+
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
   return (
     <Row
       align="middle"
-      className={style.weatherContainer}>
-      <Col>
+      className={style.weatherContainer}
+    >
+      <WeatherModalComponent 
+        weatherInfo={weatherInfo}
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+      />
+      <Col 
+        onClick={showModal}
+        style={{ cursor:"pointer" }}
+      >
         <img 
-          src={cityInfo.cityIcon}
+          src={weatherInfo.icon}
           className={style.weatherIcon}
           alt="weather-icon"
         />
       </Col>
-      <Col>
+      <Col 
+        onClick={showModal}
+        style={{ cursor:"pointer" }}
+      >
         <Row justify="center">
             <Text className={style.weatherTitle}>
-              {/* {cityInfo.cityName} */}
-              Minsk
+              {weatherInfo.countryName}
             </Text>
             <Text className={style.weatherTitle}>
-              {/* {cityInfo.cityTemperature} */}
-              12 C
+              {`${weatherInfo.temperature} °C`}
             </Text>
         </Row>
         <Row className={style.weatherSubtitle}>
-          {/* {cityInfo.cityDiscr} */}
-          Mostly Rain, without snow
+          {weatherInfo.discription}
         </Row>
       </Col>
     </Row>
