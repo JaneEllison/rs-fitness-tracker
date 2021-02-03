0866f22e376a3862ee507a34fc1016b96022df47
Velidoss <velidoss11@gmail.com>
2021-02-03 20:39:00 +0200

    feat: added fix to food table

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
 
     ],
     'no-underscore-dangle': 'off',
+    'linebreak-style': 0,
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/mouse-events-have-key-events': 'off',
