0c81350e312062524ae49a08502b5251fff85a48
Velidoss <velidoss11@gmail.com>
2021-02-08 21:01:56 +0200

    feat: added load spinner

--- src/components/commonComponents/AuthIsLoaded.jsx
+++ src/components/commonComponents/AuthIsLoaded.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { isLoaded } from 'react-redux-firebase';
+import { Spin } from 'antd';
 import authSelector from '../../store/Selectors/authSelector';
 
 const AuthIsLoaded = ({ children }) => {
   const auth = useSelector(authSelector);
-  if (!isLoaded(auth)) return <div>...Loading...</div>;
+  if (!isLoaded(auth)) return <Spin tip="Loading...">{children}</Spin>;
   return children;
 };
 
